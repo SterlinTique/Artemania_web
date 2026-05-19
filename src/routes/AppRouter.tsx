986b87bfdbff631ed4ba8111a2001ed85d426d79
SterlinTique <jader.tique@utp.edu.co>
2026-05-18 23:50:22 -0500

    feat: agregar configuración inicial de rutas, componentes de autenticación y estilos de tema(sin implementar)

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,14 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import HomePage from "../pages/home/HomePage";
 import LoginPage from "../pages/auth/LoginPage";
+import RegisterPage from "../pages/auth/RegisterPage";
+import ForgotPasswordPage from "../pages/auth/ForgotPasswordPage";
 
 export default function AppRouter() {
     return (
         <BrowserRouter>
         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route path="/" element={<LoginPage />} />
+
+            <Route
+            path="/register"
+            element={<RegisterPage />}
+            />
+
+            <Route
+            path="/forgot-password"
+            element={<ForgotPasswordPage />}
+            />
         </Routes>
         </BrowserRouter>
     );
